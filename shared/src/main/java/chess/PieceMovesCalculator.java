be8b1f9bfc5a6b6eaad9b05894f993d25458bffe
Chloe Sneddon <csneddo1@byu.edu>
2024-09-24 10:07:21 -0600

    Implemented piece rules in a different class
    moved switch statement from ChessPiece class to PieceMovesCalculator to decouple

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,10 +1,41 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+public class PieceMovesCalculator {
 
-public interface PieceMovesCalculator {
-    //    private ChessPiece piece;
-    Collection <ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+        ChessPiece.PieceType pieceType = piece.getPieceType();
 
+        switch (pieceType) {
+            case KING:
+                KingMovesCalculator king = new KingMovesCalculator();
+                moves = king.CalculateMoves(board, piece, myPosition);
+                break;
+            case PAWN:
+                PawnMoveCalculator pawn = new PawnMoveCalculator();
+                moves = pawn.CalculateMoves(board, piece, myPosition);
+                break;
+            case QUEEN:
+                QueenMovesCalculator queen = new QueenMovesCalculator();
+                moves = queen.CalculateMoves(board, piece, myPosition);
+                break;
+            case BISHOP:
+                BishopMovesCalculator bishop = new BishopMovesCalculator();
+                moves = bishop.CalculateMoves(board, piece, myPosition);
+                break;
+            case KNIGHT:
+                KnightMovesCalculator knight = new KnightMovesCalculator();
+                moves = knight.CalculateMoves(board, piece, myPosition);
+                break;
+            case ROOK:
+                RookMovesCalculator rook = new RookMovesCalculator();
+                moves = rook.CalculateMoves(board, piece, myPosition);
+                break;
+        }
+        return moves;
+    }
 }
